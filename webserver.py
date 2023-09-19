ca1c84c8af78ad4cccfa7c31e56871485b8f0c2c
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2023-09-18 19:52:00 -0700

    test: webserver

--- webserver.py
+++ webserver.py
@@ -10,26 +10,26 @@ from urllib.parse import parse_qsl, urlparse
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
-    @cached_property
-    def url(self):
-        return urlparse(self.path)
+    # @cached_property
+    # def url(self):
+    #     return urlparse(self.path)
 
-    @cached_property
-    def query_data(self):
-        return dict(parse_qsl(self.url.query))
+    # @cached_property
+    # def query_data(self):
+    #     return dict(parse_qsl(self.url.query))
 
-    @cached_property
-    def post_data(self):
-        content_length = int(self.headers.get("Content-Length", 0))
-        return self.rfile.read(content_length)
+    # @cached_property
+    # def post_data(self):
+    #     content_length = int(self.headers.get("Content-Length", 0))
+    #     return self.rfile.read(content_length)
 
-    @cached_property
-    def form_data(self):
-        return dict(parse_qsl(self.post_data.decode("utf-8")))
+    # @cached_property
+    # def form_data(self):
+    #     return dict(parse_qsl(self.post_data.decode("utf-8")))
 
-    @cached_property
-    def cookies(self):
-        return SimpleCookie(self.headers.get("Cookie"))
+    # @cached_property
+    # def cookies(self):
+    #     return SimpleCookie(self.headers.get("Cookie"))
 
     def do_GET(self):
         self.send_response(200)
@@ -41,13 +41,10 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return f"""
     <h1> Hola Web </h1>
     <p>  {self.path}         </p>
-    <p>  {self.headers}      </p>
-    <p>  {self.cookies}      </p>
-    <p>  {self.query_data}   </p>
 """
 
 
 if __name__ == "__main__":
     print("Server starting...")
-    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
+    server = HTTPServer(("0.0.0.0", 80), WebRequestHandler)
     server.serve_forever()
